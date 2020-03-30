5518242a754761738b94618e0f422c9244d56c9b
PC-73 <huangfengrui>
2020-03-30 11:48:39 +0800

    git ninit

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express()
+const request = require('./utils/request')
+
+app.set('view engine', 'pug')
+
+app.get('/', async (req, res) => {
+  const shareData = await request({
+    url: 'https://www.hzmra.com/web/live/get/205695763420807168'
+  })
+  res.render('index', {
+    title: shareData.title,
+    imageUrl: shareData.imageUrl,
+    content: shareData.introduce,
+    link: shareData.shareLink
+  })
+})
+
+app.listen(8888, () => {
+  console.log('已启动')
+})
